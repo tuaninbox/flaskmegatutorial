7b67485a9a96691bdc613b1c5ce978e22e594cd9
Tuan Hoang <tuaninbox@yahoo.com>
2020-03-07 22:38:06 +0800

    template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import megaapp
 
 @megaapp.route('/')
 @megaapp.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'username':'Tuan'}
+	return render_template('index.htm',title='Home Page',user=user)
